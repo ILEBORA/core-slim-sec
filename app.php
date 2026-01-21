292835aa2a9d5bda6e47176ef42d67cc029ae26b
ilebora <ileboratech@gmail.com>
2026-01-21 18:45:18 +0300

    Test AssetPublisher

--- app.php
+++ app.php
@@ -4,8 +4,8 @@
  *  BoraSlim Secure Distribution
  *  Framework:  ilebora/core-slim-sec
  *  Version:    2.1.8
- *  Build ID:   717787ECCCF0
- *  Timestamp:  2026-01-21 18:21:35
+ *  Build ID:   DDE0ED12BDEC
+ *  Timestamp:  2026-01-21 18:42:42
  *  License:    Proprietary - Unauthorized modification or redistribution prohibited.
  *  Contact:
  *  support@boracore.co.ke
